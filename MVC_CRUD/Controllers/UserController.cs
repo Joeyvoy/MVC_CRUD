36e6e4ab18c7b7d87222e14977bec610dda1a5dd
dell <dell@DESKTOP-SH2OB9S>
2021-05-26 22:55:21 +0800

    Add project files.

---
+++ MVC_CRUD/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD.DataContext;
+using MVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly CrudDbContext _cruddbcontext;
+
+        public UserController(CrudDbContext crudDbContext)
+        {
+            _cruddbcontext = crudDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ContactTrace(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                _cruddbcontext.Users.Add(u);
+                _cruddbcontext.SaveChanges();
+                return RedirectToAction("Allusers");
+            }
+            return View("Index");
+        }
+        public IActionResult AllUsers()
+        {
+            return View(_cruddbcontext.Users.ToList());
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var user = _cruddbcontext.Users.FirstOrDefault(u => u.UserId == id);
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult Edit(User u)
+        {
+            if (u == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid) 
+            {
+                _cruddbcontext.Update(u);
+                _cruddbcontext.SaveChanges();
+                return RedirectToAction("Allusers");
+            }
+            return View("Edit");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var user = _cruddbcontext.Users.FirstOrDefault(u => u.UserId == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _cruddbcontext.Remove(user);
+            _cruddbcontext.SaveChanges();
+
+            return RedirectToAction("Allusers");
+        }
+    }
+}
---
+++ MVC_CRUD/Migrations/20210526143439_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_CRUD.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date_TimeIn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temperature = table.Column<double>(type: "float", nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
